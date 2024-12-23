a97c208f9543ed3117109b2b283eafd71426282c
ShubhamPadhi <165741931+ShubhamPadhi@users.noreply.github.com>
2024-12-23 23:01:34 +0530

    Update App1.py

--- App1.py
+++ App1.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
 
-# Load the trained model and vectorizer (ensure the paths are correct)
 NB_model = joblib.load('NB_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
@@ -16,13 +15,12 @@ def predict():
     if request.method == 'POST':
         input_text = request.form['input_text']
         
-        # Transform the input text using the loaded vectorizer
         input_TFIDF = vectorizer.transform([input_text])
         
-        # Predict the sentiment using the loaded model
+       
         predicted_sentiment = NB_model.predict(input_TFIDF)
         
-        # Return the result
+    
         return render_template('index2.html', prediction=predicted_sentiment[0])
     else:
         return "This route only accepts POST requests"
